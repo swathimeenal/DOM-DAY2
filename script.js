7c1a540f6f85cd440738bb88382515a81c5d0f5c
swathimeenal <swathimeenal1994@gmail.com>
2023-08-28 10:44:13 +0530

    getElement

---
+++ script.js
@@ -0,0 +1,15 @@
+//getElementbyId
+const res = document.getElementById("container");
+console.log(res);
+//getElementByClassName
+const res1 = document.getElementsByClassName("main");
+console.log(res1);
+//getElementByTagName
+const res2 = document.getElementsByTagName("p");
+console.log(res2);
+//Query selectorAll
+const res4 = document.querySelectorAll("div");
+console.log(res4);
+//Query selector
+const res5 = document.querySelector("div");
+console.log(res5);
