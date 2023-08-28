4a9f65bc8dd81b93aa9264d08f98b1cb0d849dd1
swathimeenal <swathimeenal1994@gmail.com>
2023-08-28 11:38:09 +0530

    age

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //getElementbyId
-const res = document.getElementById("container");
+/*const res = document.getElementById("container");
 console.log(res);
 //getElementByClassName
 const res1 = document.getElementsByClassName("main");
@@ -13,3 +13,38 @@ console.log(res4);
 //Query selector
 const res5 = document.querySelector("div");
 console.log(res5);
+*/
+//onclick function
+/*function foo(){
+    const result = document.getElementById("email").value;
+console.log(result);
+}
+*/
+/* function Check(){
+    const name = document.getElementById("email").value;
+    console.log(name)
+       if(name==swathimeenal1994@gmail.com)
+       {
+        console.log("Valid-User")
+       }
+       else{
+        console.log("InValid-User")
+       };
+}
+*/
+function calculateAge(){
+    const dob =new Date(document.getElementById("dob").value);
+    const today = new Date();
+
+    if(isNaN (dob.getTime())){
+        document.getElementById("output").innerHTML="Please Enter the Valid Date";
+        return error;
+    }
+    let age = today.getFullYear()- dob.getFullYear();
+    const monthDiff = today.getMonth()-dob.getMonth();
+    if(monthDiff<0||(monthDiff===0 && today.getDate() < dob.getDate()))
+    {
+        age--;
+    }
+    document.getElementById("output").innerHtml="you are"+ age+ " Years Old.";
+}
